e4940ddf87e7b4e6e58510bb62250ac931d57302
Dawid <dawid.kotula@interia.pl>
2026-05-16 10:15:27 +0200

    #2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 
 function App() {
     let email = "mojemail@gmail.com"
+
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
+
     return (
         <div className="App">
-            <h1>Hello. My e mail is {email}</h1>
+            <h1>Hello. My email is {email}</h1>
+            <input type="text" onChange={handleChange}/>
+
         </div>
     );
 }
