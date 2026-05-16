2ef054e524135a2729763649490629fb54aa03e2
Dawid <dawid.kotula@interia.pl>
2026-05-16 12:08:06 +0200

    #5 Logowanie

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState("mojemail@gmail.com");
+    const [login, setLogin] = useState(true);
 
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
+
+    // let login = true;
+    // let iAmIn = false;
+
     let komunikat = "";
+
+    function pop() {
+        alert(email);
+        // iAmIn = true;
+        setLogin(false)
+
+    }
+
+    function logout() {
+        // iAmIn = false;
+        setEmail("");
+        setLogin(true);
+    }
 
     if (email.length <= 4) {
         komunikat = "Twój adres jest za krótki";
     }
     if (email.length > 4 && email.length <= 10) {
         komunikat = "Twój adres jest ok";
-    }
 
+    }
     if (email.length >= 10) {
         komunikat = "Twój adres jest za długi";
     }
@@ -26,9 +44,19 @@ function App() {
     return (
         <div className="App">
 
-            <h1>Hello. My email is {email}</h1>
-            <div>{komunikat}</div>
-            <input type="text" value={email} onChange={handleChange}/>
+            {login && <div>
+                <h1>Hello. My email is {email}</h1>
+                <div>{komunikat}</div>
+                <input type="text" value={email} onChange={handleChange}/>
+                <button onClick={pop}>Zaloguj</button>
+            </div>}
+
+            {!login && <div>
+                Hi {email}!
+                <button onClick={logout}>Wyloguj</button>
+
+
+            </div>}
 
         </div>
     );
